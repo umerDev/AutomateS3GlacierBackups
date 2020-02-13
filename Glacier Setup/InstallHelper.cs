eb0e3fd5a1ae7f914c80bd2026a3d3f94ac9a6c0
Raja Umer <umer.raja@fujitsu.com>
2020-02-13 14:30:56 +0000

    Refactoring

--- Glacier Setup/InstallHelper.cs
+++ Glacier Setup/InstallHelper.cs
@@ -2,21 +2,108 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Glacier_Setup
 {
     class InstallHelper
     {
         private static readonly string AWSBackupRootFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Documents", "AWSBackupRootFolder");
-        private static string configPath = Path.Combine(AWSBackupRootFolder, "config.json");
-       
+        private static readonly string configurationFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), AWSBackupRootFolder, "Configuration");
+        private static readonly string usersS3DownloaderPath = Path.Combine(configurationFolder, "S3Downloader.bat");
+        private static string configPath = Path.Combine(configurationFolder, "config.json");
+
         private string InstallToast = "Install-Module -Name BurntToast";
         private string InstallAWSTools = "Install-Module -Name AWS.Tools.Installer -Force";
         private string InstallScope = "-Scope CurrentUser";
 
         public string AWSVaultName;
         public string AWSRegion;
+        public List<string> BucketNames = new List<string>();
 
+        public InstallHelper()
+        {
+            Directory.CreateDirectory(AWSBackupRootFolder);
+            Directory.CreateDirectory(configurationFolder);
+        }
+
+        public void BucketsToBackup()
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+            WriteBatchDownloader();
+        }
+
+        private void WriteBatchDownloader()
+        {
+            using (StreamWriter file = new StreamWriter(usersS3DownloaderPath))
+            {
+                file.WriteLine("@echo off");
+                file.WriteLine("title AWS Backup");
+                file.WriteLine("echo Welcome to AWS Backup");
+                file.WriteLine("color a");
+                file.WriteLine();
+                file.Close();
+            }
+
+            foreach (var i in BucketNames)
+            {
+                using (StreamWriter sw = File.AppendText(usersS3DownloaderPath))
+                {
+                    sw.WriteLine($@"echo You are going to copy bucket {i}");
+                    sw.WriteLine($@"aws s3 sync s3://{i} %UserProfile%\Documents\AWSBackupRootFolder\AWSArchives\Today");
+                    sw.WriteLine("echo Finished download");
+                    sw.WriteLine("setlocal enableextensions");
+                    sw.WriteLine(@"set todaysDate=%DATE:/=_%");
+                    sw.WriteLine($@"Rename %UserProfile%\Documents\AWSBackupRootFolder\AWSArchives\Today {i}_""%todaysDate%"" ");
+                    sw.WriteLine("echo Renamed Folder");
+                    sw.WriteLine();
+                }
+            }
+        }
+
+        private void BucketInput()
+        {
+            Console.WriteLine("\nEnter Bucket Name");
+            var input = Console.ReadLine();
+            if (String.IsNullOrEmpty(input))
+                return;
+            BucketNames.Add(input);
+            Console.WriteLine("Bucket added");
+            Console.WriteLine("\n\nPress enter to continue");
+            Console.ReadKey();
+        }
+
+        private bool MainMenu()
+        {
+            try
+            {
+                Console.Clear();
+                Console.WriteLine("\r\n Select an option: ");   
+                Console.WriteLine("1) Add new bucket...");
+                Console.WriteLine("2) Save \n");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        BucketInput();
+                        return true;
+                    case "2":
+                        return false;
+                    default:
+                        return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+        
         public void InstallAWSCLI()
         {
             Process CMDprocess = new System.Diagnostics.Process();
@@ -52,7 +139,6 @@ namespace Glacier_Setup
             StartProcessInfo.FileName = @"C:\Windows\SysWOW64\WindowsPowershell\v1.0\powershell.exe";
             StartProcessInfo.Verb = "runas";
             StartProcessInfo.Arguments = $"{InstallToast} {InstallScope}";
-
             CMDprocess.StartInfo = StartProcessInfo;
             CMDprocess.Start();
             CMDprocess.WaitForExit();
@@ -72,7 +158,7 @@ namespace Glacier_Setup
                     EnterAWSVaultName();
                 }
             }
-            Directory.CreateDirectory(AWSBackupRootFolder);
+
             if (File.Exists(configPath))
                 File.Delete(configPath);
 
